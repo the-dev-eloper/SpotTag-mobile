b1231801b64c7f0baf911b612e96635a5de60c6e
the-dev-eloper <imgnath28@gmail.com>
2021-09-10 12:34:52 +0530

    Bug Screen

--- frontend/app/components/bugs/BugItemsPane.js
+++ frontend/app/components/bugs/BugItemsPane.js
@@ -10,13 +10,127 @@ import {
     Image
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+var { height, width } = Dimensions.get('window');
+import navBarStyles from '../../styles/navbar';
+import data from '../../containers/data';
+
 class BugItemsPane extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+           
+        }
+    }
+
+    renderNavigationBar() {
+
+        return (
+            <View style={navBarStyles.navbar}>
+
+                <TouchableOpacity
+                    accessible={true}
+                    testID={"Menu"}
+                    accessibilityLabel={"menu"}
+                    onPress={() => this.onPressed()}
+                >
+                    <Icon style={navBarStyles.navBarMenu} name='bars' />
+                </TouchableOpacity>
+
+                <Text style={navBarStyles.navBarTitle}>
+                    Bug Screen
+                </Text>
+            </View>
+        )
+    }
+
+    onPressed() {
+        Alert.alert("Works!");
+    }
+
+    renderHeadingTexts() {
+        return (
+            <View>
+                <Text style={styleTab.headText}>
+                    All Bugs
+                </Text>
+            </View>
+        )
+    }
+
+    renderList() {
+        return(
+            <FlatList
+                style={{ flex:1 }}
+                data={data.bugs}
+                renderItem={({ item }) => <Item item={item}/>}
+                keyExtractor={item => item.email}
+            />
+        );
+    }
+
+    onSelected(name) {
+        Alert.alert(name)
+    }     
+
     render() {
+
+        Item = ({ item }) => {
+            return (
+
+                <TouchableOpacity onPress={() => this.onSelected(item.name)}>
+                    <View style={styleTab.listItem}>
+
+                        <View style={styleTab.listHeader}>
+                            <Text
+                                style={{
+                                    fontWeight:"bold",
+                                    fontSize: 20,
+                                    color: "#FFFFFF"
+                                }}
+                            >
+                                {item.name}
+                            </Text>
+                        </View>
+
+                        <View style={styleTab.listContent}>
+
+                            <Text
+                                style={{
+                                    fontWeight:"bold",
+                                    fontSize: 15,
+                                    color: "#FFFFFF"
+                                }}
+                            >
+
+                                Category: {item.category} {"\n"}
+                                Language: {item.language} {"\n"}
+                                Reason: {item.reason} {"\n"}
+                                Solution: {item.solution}
+                            </Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            );
+        }
+
         return (
             <View style={styleTab.mainWrap}>
-                <Text>
-                    All Bugs Screen
-                </Text>
+
+                <View style={styleTab.headerWrap}>
+                    {this.renderNavigationBar()}
+                </View>
+
+                <View style={{ flexDirection: 'column' }}>
+                    {this.renderHeadingTexts()}
+                </View>
+
+                <View style={styleTab.listStyle}>
+                    {this.renderList()}
+                </View>
             </View>
         )
     }
@@ -27,7 +141,42 @@ export default BugItemsPane;
 const styleTab = StyleSheet.create({
     mainWrap: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        height: '100%',
+        backgroundColor: '#F7F7F7',
+    },
+    headerWrap: {
+        flexDirection: 'row',
+        paddingTop: 40
+    },
+    headText: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        lineHeight: 22,
+        color: '#25282B',
+        marginTop: height * 0.015,
+        marginStart: width * 0.1
+    },
+    listStyle: {
+        flex: 1,
+        backgroundColor: '#F7F7F7',
+        marginTop: 20
+    },
+    listItem:{
+        margin: 10,
+        padding: 10,
+        backgroundColor: "#0000FF",
+        width: "90%",
+        height: height * 0.2,
+        alignSelf: "center",
+        borderRadius: 10
+    },
+    listHeader: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        padding: 10,
+    },
+    listContent: {
+        padding: 10,
+        paddingLeft: 50
     }
 })
--- frontend/app/containers/app.js
+++ frontend/app/containers/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import LanguageItemsPane from '../components/languages/LanguageItemsPane';
+import BugItemsPane from '../components/bugs/BugItemsPane';
 
 export default class SpotTag extends Component {
 
     render() {
         return (
-            <LanguageItemsPane />
+            <BugItemsPane />
         );
     }
 }
