fdcc1ce2e0204d1e330865549ba7c018406cdc07
the-dev-eloper <imgnath28@gmail.com>
2021-09-14 11:15:51 +0530

    Changed _id to 'id'

--- backend/models/languageModel.js
+++ backend/models/languageModel.js
@@ -2,15 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-const languageSchema = mongoose.Schema(
-    {
-        name: { type: String, required: true, unique: true },
-        image: { type: String },
-        developer: { type: String },
-        stableRelease: { type: String },
-        firstAppeared: { type: String },
-        bugList: { type: Array },
-        dateCreated: { type: Date, default: Date.now()}
-    });
+const languageSchema = mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    image: { type: String },
+    developer: { type: String },
+    stableRelease: { type: String },
+    firstAppeared: { type: String },
+    bugList: { type: Array },
+    dateCreated: { type: Date, default: Date.now()}
+});
+
+languageSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+languageSchema.set('toJSON', {
+    virtuals: true
+});
 
 exports.Language = mongoose.model('Language', languageSchema);
