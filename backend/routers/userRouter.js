021c7dc066f8b46e47ec6ecd74e50e3a6055beb3
the-dev-eloper <imgnath28@gmail.com>
2021-09-14 19:39:11 +0530

    Delete API for user

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -68,6 +68,22 @@ userRouter.put(`/:id`, async (req, res) => {
     }
 });
 
+userRouter.delete(`/:id`, async (req, res) => {
+
+    User.findByIdAndRemove(req.params.id)
+        .then((deletedUser) => {
+            if(deletedUser) {
+                return res.status(200).json({ success: true, message: 'Deleted Successfully' })
+            } else {
+                return res.status(404).json({ success: false, message: 'User not found' })
+            }
+        })
+
+        .catch((err) => {
+            return res.status(400).json({ success: false, message: err.message })
+        })
+});
+
 userRouter.post(`/login`, async (req, res) => {
 
     const user = await User.findOne({ email: req.body.email });
