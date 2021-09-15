981f67bb3bef7ac0b1dfba512a15bbb7fe8db7a0
the-dev-eloper <imgnath28@gmail.com>
2021-09-15 19:04:36 +0530

    Excluding routs from authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,14 +1,29 @@
 const expressJwt = require('express-jwt');
-require('dotenv/config');
 
 function authJWT() {
 
     const secret = process.env.secret;
+    const api = process.env.api;
 
     return expressJwt({
         secret,
-        algorithms: ['HS256']
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
+    }).unless({
+        path: [
+            { url: /\/api\/v1\/languages/ , methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ]
     })
+}
+
+async function isRevoked(req, payload, done) {
+    if(!payload.isAdmin) {
+        done(null, true)
+    }
+
+    done();
 }
 
 module.exports = authJWT;
