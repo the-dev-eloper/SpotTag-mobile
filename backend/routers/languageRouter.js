db1e8ca4e0cd3a6c4ad666304f1399394ed50f1b
the-dev-eloper <imgnath28@gmail.com>
2021-09-22 12:32:44 +0530

    Validating uploaded file types

--- backend/routers/languageRouter.js
+++ backend/routers/languageRouter.js
@@ -5,12 +5,28 @@ const { Language } = require("../models/languageModel");
 
 const languageRouter = express.Router();
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/uploads');
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if (isValid) {
+            uploadError = null;
+        }
+
+        cb(uploadError, 'public/uploads');
     },
+
     filename: function (req, file, cb) {
         const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+
         cb(null, `${fileName}-${Date.now()}.${extension}`);
     },
 });
