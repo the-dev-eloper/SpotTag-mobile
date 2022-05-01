db38a3fda9ac56299a019fbcb297e714ad95b53b
Gokulnath D <imgnath28@gmail.com>
2022-05-01 12:59:26 +0530

    Frontend refactoring(class to function component)

--- frontend/app/containers/app.js
+++ frontend/app/containers/app.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { NavigationContainer } from "@react-navigation/native";
-import Main from '../navigators/MainNavigator';
+const SpotTag = () => {
+    return (
+        <View style={styles.homeContainer}>
+            <Text style={styles.titleText}>
+                Spot-<Text style={styles.titleSpan}>Tag</Text>
+            </Text>
+        </View>
+    );
+};
 
-export default class SpotTag extends Component {
-
-    render() {
-        return (
-            <NavigationContainer>
-                <Main />
-            </NavigationContainer>
-        );
+const styles = StyleSheet.create({
+    homeContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titleText: {
+        fontSize: 50,
+        color: '#7B3F00'
+    },
+    titleSpan: {
+        color: '#483C32'
     }
-}
+});
+
+export default SpotTag;
